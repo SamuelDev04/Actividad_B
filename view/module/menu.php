eab61f542c2e99d90b8b54fde89a2592faace436
Samuel Y <syepes779@misena.edu.co>
2022-05-23 15:42:25 -0500

    Ingreso de lo ya hecho

---
+++ view/module/menu.php
@@ -0,0 +1,32 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <!--<li class="header"></li>-->
+        <li class="">
+          <a href="index.php?ruta=aprendiz">
+            <i class="fa fa-pencil"></i> 
+            <span>Aprendiz 😫</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="index.php?ruta=matricula">
+            <i class="fa fa-mortar-board"></i>
+            <span>Matricula 😀</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="index.php?ruta=usuario">
+            <i class="fa fa-user"></i>
+            <span>Usuarios 🥵</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <div id="ohsnap"></div>
