739498cdcd485f968104b8c9004b7bef82f92514
Samuel Y <syepes779@misena.edu.co>
2022-05-23 20:21:49 -0500

    Me los activa, pero no se como desactivarlos ni como hacer para que me envie a la pagina adecuada

--- view/module/menu.php
+++ view/module/menu.php
@@ -2,23 +2,23 @@
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-      
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-        <!--<li class="header"></li>-->
-        <li class="">
+        <li class="header">Main Navigation</li>
+
+        <li class="inutil1 treeview" onclick="validateSidebarAp(event)">
           <a href="index.php?ruta=aprendiz">
             <i class="fa fa-pencil"></i> 
             <span>Aprendiz 😫</span>
           </a>
         </li>
-        <li class="">
+        <li class="inutil2 treeview" onclick="validateSidebarMa(event)">
           <a href="index.php?ruta=matricula">
             <i class="fa fa-mortar-board"></i>
             <span>Matricula 😀</span>
           </a>
         </li>
-        <li class="">
+        <li class="inutil3 treeview" onclick="validateSidebarUs(event)">
           <a href="index.php?ruta=usuario">
             <i class="fa fa-user"></i>
             <span>Usuarios 🥵</span>
